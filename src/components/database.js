f7c48a3a2bc656b71a2a6f05e70fafa84a38ad9a
Sugam Adhikari <sugam.19217113@gmail.com>
2022-03-23 18:43:20 +0545

    files added

---
+++ src/components/database.js
@@ -0,0 +1,45 @@
+
+export const Leaderboard = [
+    {
+        name: "Shawn Hanna",
+        class:"Tweleve",
+        score : 1550,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2022-02-10"
+    },
+    {
+        name: "Fidel Hand",
+        class:"Tweleve",
+        score : 2310,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2021-01-01"
+    },
+    {
+        name: "Bessie Hickle",
+        class:"Tweleve",
+        score : 350,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2021-08-17"
+    },
+    {
+        name: "Adella Wunsch",
+        class:"Eleven",
+        score : 2100,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2021-10-23"
+    },
+    {
+        name: "Clair O'Connell",
+        class:"Tweleve",
+        score : 1250,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2022-01-22"
+    },
+    {
+        name: "Kameron Prosacco",
+        class:"Tweleve",
+        score : 5250,
+        img: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+        dt: "2022-01-21"
+    }
+]
